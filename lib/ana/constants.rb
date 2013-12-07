55bea01e5abeec94dfe6911e3e2e0862d49ec723
Juanito Fatas <katehuang0320@gmail.com>
2013-12-07 15:14:41 +0800

    :stuck_out_tongue_closed_eyes: Update comments in constants.rb.

--- lib/ana/constants.rb
+++ lib/ana/constants.rb
@@ -1,6 +1,6 @@
 module Ana
   module Scalars
-    TTL = 900 # Every 900s updates the json.
+    TTL = 900 # ~/gemjsons/gemname/*.json expires time.
     GEM_NOT_FOUND      = 'This rubygem could not be found.'
     GEM_DOES_NOT_EXIST = 'This gem does not exist.'
     GEM_VER_NOT_FOUND  = 'This version could not be found.'
@@ -10,7 +10,7 @@ module Ana
     BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = [*30..37]
 
     # $ curl https://rubygems.org/api/v1/gems/rails.json
-    # uris of above json
+    # URIs of above json
     URI_TYPE = {
       'proj' =>  'project_uri',
       'lib'  =>  'gem_uri',
